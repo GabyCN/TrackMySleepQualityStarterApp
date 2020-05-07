e0ebaa362e15a9320fe754222fce5039b8097f9e
GabyCN <Gabynoestaquieta@gmail.com>
2020-05-07 15:39:09 +0200

    Click

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -21,14 +21,11 @@ import android.view.ViewGroup
 //import android.widget.ListAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.example.android.trackmysleepquality.R
-import com.example.android.trackmysleepquality.convertDurationToFormatted
-import com.example.android.trackmysleepquality.convertNumericQualityToString
 import com.example.android.trackmysleepquality.database.SleepNight
 import com.example.android.trackmysleepquality.databinding.ListItemSleepNightBinding
 
 
-class SleepNightAdapter : androidx.recyclerview.widget.ListAdapter<SleepNight, SleepNightAdapter.ViewHolder>(SleepNightDiffCallback()) {
+class SleepNightAdapter (val clickListener:SleepNightListener) : androidx.recyclerview.widget.ListAdapter<SleepNight, SleepNightAdapter.ViewHolder>(SleepNightDiffCallback()) {
 
 
     /*var data = listOf<SleepNight>()
@@ -40,10 +37,11 @@ class SleepNightAdapter : androidx.recyclerview.widget.ListAdapter<SleepNight, S
     override fun getItemCount() = data.size*/
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = getItem(position)
-
+        holder.bind(getItem(position)!!, clickListener)
+        /*val item = getItem(position)
+        holder.bind(item)*/
         //val res= holder.itemView.context.resources
-        holder.bind(item)
+
         /*if (item.sleepQuality <= 1) {
             holder.textView.setTextColor(Color.RED)
         } else {
@@ -58,8 +56,9 @@ class SleepNightAdapter : androidx.recyclerview.widget.ListAdapter<SleepNight, S
 
     class ViewHolder private constructor(val binding: ListItemSleepNightBinding) : RecyclerView.ViewHolder (binding.root) {
 
-        fun bind(item: SleepNight) {
+        fun bind(item: SleepNight, clickListener: SleepNightListener) {
             binding.sleep=item
+            binding.clickListener=clickListener
             binding.executePendingBindings()
     }
         companion object {
@@ -82,4 +81,8 @@ class SleepNightDiffCallback : DiffUtil.ItemCallback<SleepNight>(){
         return oldItem == newItem
     }
 
+}
+
+class SleepNightListener (val clickListener: (sleepId:Long) -> Unit) {
+    fun onClick(night: SleepNight)=clickListener(night.nightId)
 }
